3053b5d837fa1f95f6c69173f2774a60d1085b0f
mikyasnega70 <mikenega70@gmail.com>
2025-06-29 17:23:22 +0300

    intro_to_db

---
+++ MySQLServer.py
@@ -0,0 +1,33 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',  
+            user='root',       
+            password='password'  
+        )
+        
+        if connection.is_connected():
+            print("Connected to MySQL server successfully!")
+            
+            cursor = connection.cursor()
+            
+            create_db_query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+    
+            cursor.execute(create_db_query)
+            
+            print("Database 'alx_book_store' created successfully or already exists.")
+        
+    except Error as e:
+        print(f"Error: {e}")
+    
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed.")
+
+if __name__ == "__main__":
+    create_database()
